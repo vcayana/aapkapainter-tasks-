62e5a91372e95151942418d8a2cf8b9e9be9b774
vcayana <84145231+vcayana@users.noreply.github.com>
2022-02-09 23:02:38 +0530

    Create individual_scores.py

---
+++ individual_scores.py
@@ -0,0 +1,10 @@
+def find_score(scores):
+    score=[0,0]
+    striker=0
+    non_striker=1
+    for run in scores:
+        score[striker]=score[striker]+run
+        if run%2!=2:
+            striker,non_striker=non_striker,striker
+    return "p1:",score[0],"P2:",score[1]
+print(find_score([1,2,3,2,1])
