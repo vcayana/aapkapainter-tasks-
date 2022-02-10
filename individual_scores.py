73ad200b7fcd78e4f1ca877553579380db9b50d1
vcayana <84145231+vcayana@users.noreply.github.com>
2022-02-10 07:01:20 +0530

    Update individual_scores.py

--- individual_scores.py
+++ individual_scores.py
@@ -7,4 +7,4 @@ def find_score(scores):
         if run%2!=2:
             striker,non_striker=non_striker,striker
     return "p1:",score[0],"P2:",score[1]
-print(find_score([1,2,3,2,1])
+print(find_score([1,2,3,2,1]))
